2c586f157e2b1fd98c93e614e395a3e51438d4f2
Thagoo <lohitgowda56@gmail.com>
2019-10-14 13:58:03 +0000

    rolex: Set DISABLE_EAP_PROXY to break wpa_supplicant dependency on qmi

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -244,6 +244,7 @@ WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_DRIVER_FW_PATH_P2P := "p2p"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+DISABLE_EAP_PROXY := true
 
 # Inherit from the proprietary version
 -include vendor/xiaomi/rolex/BoardConfigVendor.mk
